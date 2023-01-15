8cea9cb1e82d939e95eab4ff737fd6c3465539eb
Christian Dela Rosa <c.delarosa1104@student.nu.edu>
2023-01-15 13:51:44 -0800

    Pesky Underscores Again in app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ def index():
 
 @app.route('/predict', methods=['POST'])  # The user input is processed here
 def predict():
-    math_score = request.form['math score']
-    reading_score = request.form['reading score']
-    writing_score = request.form['writing score']
+    math_score = request.form['math_score']
+    reading_score = request.form['reading_score']
+    writing_score = request.form['writing_score']
     pred = model.predict(np.array([[math_score,reading_score,writing_score]]))
     print(pred)
     return render_template('index.html', predict=str(pred))
